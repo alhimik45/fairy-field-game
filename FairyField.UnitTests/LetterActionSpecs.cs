c23d68b2438a736e2eca1bf50ef4679597008aa2
alhimik45 <mail@alhimik.me>
2018-03-18 22:35:51 +0700

    letter action

---
+++ FairyField.UnitTests/LetterActionSpecs.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using FairyField.DrumActions;
+using Machine.Specifications;
+using NSubstitute;
+
+namespace FairyField.UnitTests
+{
+    public class LetterActionSpecs
+    {
+        [Subject(typeof(LetterAction))]
+        public class When_act
+        {
+            Establish context = () =>
+            {
+                Input = Substitute.For<TextReader>();
+                Input.ReadLine().Returns("0", "-13", "445", "sdsd","1");
+                State = new GameState(new Word("a"));
+                Subject = new LetterAction(Input, Substitute.For<TextWriter>());
+            };
+
+            Because of = () => Subject.Act(State);
+
+            It should_open_letter = () => State.CurrentWord.View.ShouldEqual("a");
+            It should_read_5_times = () => Input.Received(5).ReadLine();
+
+            static LetterAction Subject;
+            static GameState State;
+            static TextReader Input;
+        }
+    }
+}
---
+++ FairyField/DrumActions/LetterAction.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+
+namespace FairyField.DrumActions
+{
+    public class LetterAction : IDrumAction
+    {
+        private readonly TextReader input;
+        private readonly TextWriter output;
+
+        public LetterAction(TextReader input, TextWriter output)
+        {
+            this.input = input;
+            this.output = output;
+        }
+
+        public void Act(GameState gameState)
+        {
+            var opened = false;
+            int? num = null;
+            while (!opened || num == null)
+            {
+                output.WriteLine("Скажите номер буквы");
+                num = int.TryParse(input.ReadLine(), out var v) ? v : (int?) null;
+                if (num != null)
+                {
+                    opened = gameState.CurrentWord.Open(num.Value);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Открывайте любую букву!";
+        }
+    }
+}
