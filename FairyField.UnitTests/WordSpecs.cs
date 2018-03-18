d4d00a4a6d3255c842125399a107973d7f482b7b
alhimik45 <mail@alhimik.me>
2018-03-18 20:04:55 +0700

    add word spec

---
+++ FairyField.UnitTests/WordSpecs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Machine.Specifications;
+
+namespace FairyField.UnitTests
+{
+    public class WordSpecs
+    {
+            [Subject(typeof(Word))]
+            public class When_created
+            {
+                Establish context = () =>
+                {
+                    Subject = new Word();
+                };
+
+                It should_have_closed_letters = () => Subject.HaveClosedLetters.ShouldBeTrue();
+
+                static Word Subject;
+            }
+    }
+}
